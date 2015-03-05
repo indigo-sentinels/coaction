1c570897ff53eade95997269dc5058f2db23c2e4
Bernard <worthybernard@gmail.com>
2015-03-05 14:10:30 -0500

    Added basic structure

--- coaction/static/js/app.js
+++ coaction/static/js/app.js
@@ -11,6 +11,13 @@ app.config(['$routeProvider', function ($routeProvider) {
   });
 }]);
 
+
+
+
+
+
+
+
 app.controller('Error404Ctrl', ['$location', function ($location) {
   this.message = 'Could not find: ' + $location.url();
 }]);
