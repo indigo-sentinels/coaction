85eab98fc60ef73a8a5e5cfe582ab6615ff541e7
Bernard <worthybernard@gmail.com>
2015-03-05 14:43:41 -0500

    Created User Controller

--- coaction/static/js/app.js
+++ coaction/static/js/app.js
@@ -32,6 +32,27 @@ app.factory('Task', function() {
 
 
 
+app.config(['$routeProvider', function($routeProvider) {
+  var routeDefinition = {
+    templateUrl: 'users/user.html',
+    controller: 'UserCtrl',
+    controllerAs: 'vm',
+    resolve: {
+      user: ['usersService', '$route', function (usersService, $route) {
+        var routeParams = $route.current.params;
+        var id = routeParams.id;
+        console.log(id);
+        return usersService.viewUser(id);
+      }]
+    }
+  };
+
+  $routeProvider.when('/users/:id', routeDefinition);
+}])
+.controller('UserCtrl', ['user', function (user) {
+  var self = this;
+  self.user = user;
+}]);
 
 app.factory('User', function() {
   return function (spec) {
